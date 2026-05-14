d198acac1034a380acda20a101fff80c5541f8b1
Long Phu <longphu257@gmail.com>
2026-05-14 11:19:20 +0700

    feat: redesign landing presentation page

--- src/pages/landing/main.tsx
+++ src/pages/landing/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { LandingPage } from './LandingPage'
 import '../../styles/landing.css'
+import './landing-motion'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
